de4f80cf8c01d9ba369d78048ba4babbee50e320
Jose Miguel Correa <69221181+jose-miguel-correa@users.noreply.github.com>
2025-04-09 12:18:05 +0000

    protractor.conf.js comment

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,6 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
+// Updated 11/06/2025
 
 const { SpecReporter } = require('jasmine-spec-reporter');
 
